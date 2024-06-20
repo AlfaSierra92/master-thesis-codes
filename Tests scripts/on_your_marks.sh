3906e0104e27f36662de86463141f2b4ae3ab152
Antonio Solida <solida.antonio@gmail.com>
2024-06-20 14:19:22 +0200

    On your marks, get set, go!

---
+++ Tests scripts/on_your_marks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Verifica che siano stati forniti i tre argomenti necessari
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <initial_sleep> <max_concurrent_messages> <message_size>"
+    exit 1
+fi
+
+tcpdump -l -n udp port 81 | while read -r line; do ./test2_2.sh $1 $2 $3; done
