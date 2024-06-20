1caadbd74952f5839e370cfbc9ec5522ffc72729
Antonio Solida <solida.antonio@gmail.com>
2024-06-20 14:44:31 +0200

    Up

--- Tests scripts/test2_2.sh
+++ Tests scripts/test2_2.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # Verifica che siano stati forniti i tre argomenti necessari
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <max_concurrent_messages> <message_size>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <initial_sleep> <max_concurrent_messages> <message_size>"
     exit 1
 fi
 
-initial_sleep_ms=1
-max_concurrent_messages=$1
-message_size=$2
+initial_sleep_s=$1
+max_concurrent_messages=$2
+message_size=$3
+ip_broadcast=192.168.100.255
+startup_received=$(cat /sys/class/net/wlp1s0/statistics/rx_packets)
 
 # Converte lo sleep iniziale da ms a secondi per il comando sleep
-initial_sleep_s=$(bc <<< "scale=3; $initial_sleep_ms / 1000")
+#initial_sleep_s=$(bc <<< "scale=3; $initial_sleep_ms / 1000")
 
 # Genera un messaggio di dimensione specificata
 message=$(head -c $message_size </dev/zero | tr '\0' 'A')
@@ -19,7 +21,12 @@ message=$(head -c $message_size </dev/zero | tr '\0' 'A')
 # Funzione per inviare messaggi
 send_messages() {
     for ((i=0; i<$max_concurrent_messages; i++)); do
-        echo -n "$message" | nc -u -w1 127.0.0.1 12345 &
+        # Generate a random port between 1024 and 65535
+        random_port=$(( ( RANDOM % 64512 ) + 1024 ))
+
+        # Send the message via UDP to the broadcast IP on the random port
+        echo -n "$message" | nc -u -w1 $ip_broadcast $random_port &
+        #echo -n "$message" | nc -u -w1 $ip_broadcast 12345 &
     done
     wait
 }
@@ -29,6 +36,7 @@ set_tx_power() {
     local power_level=$1
     # Assicurati di sostituire 'wlo1' con il tuo dispositivo di rete
     #iwconfig wlo1 txpower $power_level
+    iw wlp1s0 set txpower fixed $power_level
     echo "Potenza di trasmissione impostata a $power_level dBm"
 }
 
@@ -46,12 +54,15 @@ while true; do
     send_messages
     
     # Attendi un momento per assicurarti che i pacchetti siano stati elaborati
-    sleep 1
+    #sleep 1
     
     # Ricezione dei messaggi
     echo "Misurazione dei pacchetti ricevuti..."
-    current_received=$(cat /sys/class/net/wlo1/statistics/rx_packets)
+    current_received=$(cat /sys/class/net/wlp1s0/statistics/rx_packets)
     received=$((current_received - previous_received))
+    if [ "$received" -eq "$startup_received" ]; then
+        received=0
+    fi
     echo "Pacchetti ricevuti: $received"
 
     # Calcolo del carico di canale
@@ -61,23 +72,21 @@ while true; do
     # Aggiornamento dello stato della macchina a stati DCC e impostazione della potenza di trasmissione
     if (( $(echo "$minChannelLoad >= 0.4" | bc -l) )); then
         state="restrictive"
-        #sleep_time=$(bc <<< "scale=3; $initial_sleep_s * 2")
         sleep_time=1
-        set_tx_power 10  # Imposta a 10 dBm in stato restrictive
+        set_tx_power 500  # Imposta a 10 dBm in stato restrictive
         echo "Stato: RESTRICTIVE"
         echo $sleep_time
     elif (( $(echo "$minChannelLoad >= 0.15" | bc -l) )); then
         state="active"
-        sleep_time=$initial_sleep_s
         sleep_time=0.06
-        set_tx_power 20  # Imposta a 20 dBm in stato active
+        set_tx_power 1000  # Imposta a 20 dBm in stato active
         echo "Stato: ACTIVE"
         echo $sleep_time
     else
         state="relaxed"
         #sleep_time=$(bc <<< "scale=3; $initial_sleep_s / 2")
         sleep_time=0.04
-        set_tx_power 30  # Imposta a 30 dBm in stato relaxed
+        set_tx_power 15000  # Imposta a 30 dBm in stato relaxed
         echo "Stato: RELAXED"
         echo $sleep_time
     fi
@@ -93,4 +102,5 @@ while true; do
     sleep $sleep_time
 
     previous_received=$current_received
+    echo -e ""
 done
