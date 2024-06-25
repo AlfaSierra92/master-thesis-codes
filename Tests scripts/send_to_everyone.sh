d83bd7e4fbb44e61e6baac4b39b1059a163ff00a
Antonio Solida <solida.antonio@gmail.com>
2024-06-25 14:44:59 +0200

    Up

---
+++ Tests scripts/send_to_everyone.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scp Tests\ scripts/test2_2.sh root@rock3:/root/
+scp Tests\ scripts/test2_2.sh root@rock3_1:/root/
+scp Tests\ scripts/test2_2.sh root@rock3_2:/root/
+scp Tests\ scripts/test2_2.sh root@rock3_3:/root/
