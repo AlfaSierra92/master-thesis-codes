c654cb3e443e3b76ef39be52b2afff4861746c32
Antonio Solida <solida.antonio@gmail.com>
2024-07-24 11:33:25 +0200

    chore: Update receiver script to filter and save received packets

--- Comparison scripts/receiver.sh
+++ Comparison scripts/receiver.sh
@@ -11,4 +11,9 @@ OUTPUT_FILE="output_receiver.txt"
 
 # Avvia netcat in modalità server e salva i pacchetti ricevuti nel file di output
 #nc -lu $PORT >> $OUTPUT_FILE
-nc -u -k -l -p $PORT >> $OUTPUT_FILE
+#nc -u -k -l -p $PORT >> $OUTPUT_FILE
+nc -u -k -l -p $PORT | while read -r line; do
+    # Filtra la parte ${i};${IP} e salva nel file di output
+    # awk -F';' 'NF > 1 {print $1 ";" $2}' per evitare il print di ; aggiuntivi
+    echo "$line" | awk -F';' 'NF > 1 {print $1 ";" $2}' >> $OUTPUT_FILE
+done
