f88c008c69b3c6ee0ea66dafc901562e866cd682
Antonio Solida <solida.antonio@gmail.com>
2024-08-17 14:15:00 +0200

    chore: Refactor cx.sh script to remove unused code and improve performance

--- Final tests/cx.sh
+++ Final tests/cx.sh
@@ -10,23 +10,10 @@ message_size=$1
 sleep_time=$2
 ip_broadcast=192.168.100.255
 interface=wlp1s0
-file=output_$(date +%Y-%m-%d_%H-%M-%S).csv
 random_port=$(( ( RANDOM % 64512 ) + 1024 ))
 
 # Genera un messaggio di dimensione specificata
 message=$(head -c $message_size </dev/zero | tr '\0' 'A')
-
-# Funzione per inviare messaggi
-send_messages() {
-    for ((i=0; i<$max_concurrent_messages; i++)); do
-        # Generate a random port between 1024 and 65535
-        random_port=$(( ( RANDOM % 64512 ) + 1024 ))
-
-        # Send the message via UDP to the broadcast IP on the random port
-        echo -n "$message" | nc -u -b -w1 -q0 $ip_broadcast $random_port &
-    done
-    wait
-}
 
 # Inizia lo script
 echo "Starting..."
@@ -34,7 +21,6 @@ sleep 1
 
 while true; do
     # Invio dei messaggi
-    # send_messages
     echo -n "$message" | nc -u -b -w1 -q0 $ip_broadcast $random_port &
     sleep $sleep_time
 done
